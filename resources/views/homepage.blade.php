fe5750ceca04ca998737c27f3ab09f87e58d8e73
Louis-Cauvet <louiscauvet8@gmail.com>
2024-04-01 20:15:04 +0200

    Add Sass in project

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Page d'accueil</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/app.css">
+
+    <!-- Styles -->
+    <style>
+    </style>
+</head>
+<body>
+<div class="orange"></div>
+</body>
+</html>
